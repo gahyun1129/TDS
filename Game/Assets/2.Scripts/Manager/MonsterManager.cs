b0c593c005c8e1e84f1939255164b6e45a09fcab
GH_NoteBook <elfgh29@naver.com>
2025-03-08 18:28:24 +0900

    좀비들이 순환하며 탑을 쌓기 구현 중

--- Game/Assets/2.Scripts/Manager/MonsterManager.cs
+++ Game/Assets/2.Scripts/Manager/MonsterManager.cs
@@ -8,7 +8,7 @@ public class MonsterManager : MonoBehaviour
     private static MonsterManager instance;
     public static MonsterManager GetInstance() => instance;
 
-    List<GameObject> monsters= new List<GameObject>();
+    List<List<GameObject>> monsters= new List<List<GameObject>>();
 
     private void Awake()
     {
@@ -16,15 +16,45 @@ public class MonsterManager : MonoBehaviour
         {
             instance = this;
         }
+        monsters.Add(new List<GameObject>());
     }
 
-    public void AddMonster(GameObject _monster)
+    public void AddMonster(int _layer, GameObject _monster)
     {
-        monsters.Add(_monster);
+        monsters[_layer].Add(_monster);
     }
 
-    public bool IsLastMonster(GameObject _monster)
+    public bool IsLastMonster(int _layer, GameObject _monster)
     {
-        return monsters.Count - 1 == monsters.IndexOf(_monster);
+        return monsters[_layer].Count - 1 == monsters[_layer].IndexOf(_monster) && monsters[_layer].Count != 1;
+    }
+
+    public void MoveMonsterToNextLayer(int _prevLayer, int _nextLayer, GameObject _monster)
+    {
+        if ( monsters.Count - 1 < _nextLayer )
+        {
+            monsters.Add(new List<GameObject>());
+        }
+        monsters[_prevLayer].Remove(_monster);
+        monsters[_nextLayer].Add(_monster);
+    }
+
+    public void MoveMonsterToPrevLayer(int _curLayer, int _prevLayer, GameObject _monster)
+    {
+        monsters[_curLayer].Remove(_monster);
+        monsters[_prevLayer].Insert(0, _monster);
+    }
+
+    public bool IsFirstMonster(int _layer, GameObject _monster)
+    {
+        return monsters[_layer].IndexOf(_monster) == 0;
+    }
+
+    public void MoveMonsters(int _layer)
+    {
+        foreach(GameObject _monster in monsters[_layer])
+        {
+            _monster.GetComponent<Monster>().MoveRightDirection();
+        }
     }
 }
--- Game/Assets/2.Scripts/Monster/Monster.cs
+++ Game/Assets/2.Scripts/Monster/Monster.cs
@@ -9,10 +9,11 @@ public class Monster : MonoBehaviour
     Rigidbody2D rigidBody;
 
     bool isAttacking = false;
-    float speed = 5f;
+    bool isMoving = true;
+    float speed = 2f;
     
     float jumpForce = 5f;
-
+    int curLayer = 0;
 
     Coroutine Move;
 
@@ -26,26 +27,82 @@ public class Monster : MonoBehaviour
         Move = StartCoroutine(MonsterMove());
     }
 
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if ( collision.collider.CompareTag("Truck") || collision.collider.CompareTag("Zombie"))
+        {
+            isMoving = true;
+        }
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if ( !isAttacking )
         {
             if (collision.collider.CompareTag("Zombie") || collision.collider.CompareTag("Truck"))
             {
-                StopCoroutine(Move);
+                //StopCoroutine(Move);
+                //Move = null;
+                isMoving = false;
+
                 isAttacking = true;
                 animator.SetBool("IsAttacking", true);
 
-                MonsterManager.GetInstance().AddMonster(this.gameObject);
+                curLayer = 0;
+                MonsterManager.GetInstance().AddMonster(curLayer, this.gameObject);
+            }
+        }
+        else
+        {
+            // 0이 아닌 레이어에서 일어나는 일일 것
+            if (collision.collider.CompareTag("Truck"))
+            {
+                if (isMoving)
+                {
+                    //StopCoroutine(Move);
+                    //Move = null;
+                    isMoving = false;
+                }
+
+                animator.SetBool("IsAttacking", true);
+            }
+            else if (collision.collider.CompareTag("Zombie") && Mathf.Abs(transform.position.y - collision.transform.position.y) < 0.5f)
+            {
+                if (isMoving)
+                {
+                    //StopCoroutine(Move);
+                    //Move = null;
+                    isMoving = false;
+                }
+
+                // animator.SetBool("IsAttacking", true);
             }
         }
     }
 
     public void OnAttack()
     {
-        if (MonsterManager.GetInstance().IsLastMonster(this.gameObject))
+        if (MonsterManager.GetInstance().IsLastMonster(curLayer, this.gameObject))
         {
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+            
+            MonsterManager.GetInstance().MoveMonsterToNextLayer(curLayer, curLayer + 1, this.gameObject);
+            curLayer++;
+            
+            if ( !isMoving )
+            {
+                animator.SetBool("IsAttacking", false);
+                //Move = StartCoroutine(MonsterMove());
+                isMoving = true;
+            }
+        }
+        else if (curLayer > 0 && MonsterManager.GetInstance().IsFirstMonster(curLayer, this.gameObject))
+        {
+            // 밑의 층의 몬스터들 오른쪽으로 살짝 밈
+
+            MonsterManager.GetInstance().MoveMonsters(curLayer - 1);
+            MonsterManager.GetInstance().MoveMonsterToPrevLayer(curLayer, curLayer - 1, this.gameObject);
+            curLayer--;
+
         }
     }
 
@@ -53,10 +110,18 @@ public class Monster : MonoBehaviour
     {
         while(true)
         {
-            transform.position += Vector3.left * speed * Time.deltaTime;
+            if (isMoving)
+            {
+                transform.position += Vector3.left * speed * Time.deltaTime;
+            }
             
             yield return null;
         }
+    }
+
+    public void MoveRightDirection()
+    {
+        transform.position += Vector3.right;
     }
 
 }
